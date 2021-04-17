5c625501d37ba4cd80c8961e828c8c8b7a57170e
Даниил Павлович <danya.vshivtsev@mil.ru>
2021-04-17 23:51:27 +0500

    Interections v1

---
+++ Controllers/PhysicsController.cs
@@ -0,0 +1,29 @@
+﻿using Shooter.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shooter.Controllers
+{
+    public static class PhysicsController
+    {
+        public static void isCollide(Entity entity)
+        {
+            for (int j = entity.posX / MapController.cellSize; j < (entity.posX + MapController.cellSize)/MapController.cellSize; j++ )
+                for (int i = entity.posY / MapController.cellSize; i < (entity.posY + MapController.cellSize) / MapController.cellSize; i++)
+                {
+                    if (MapController.map[i, j] != 0)
+                    {
+                        if (entity.dirY > 0)
+                            entity.posY -= 10;
+                        if (entity.dirY < 0)
+                            entity.posY += 10;
+                        if (entity.dirX > 0)
+                            entity.posY -= 10;
+                        if (entity.dirX < 0)
+                            entity.posY += 10;
+                    }
+                }
+        }
+    }
+}
