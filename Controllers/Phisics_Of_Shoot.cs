b2e0d4171effbef764ad4cd51c70974206a1340a
Daniil.Vshivtsev@urfu.me <Daniil.Vshivtsev@urfu.me>
2021-05-04 21:58:32 +0500

    v2

--- Controllers/Phisics_Of_Shoot.cs
+++ Controllers/Phisics_Of_Shoot.cs
@@ -3,17 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Shooter.Controllers
 {
     public class Phisics_Of_Shoot
     {
         public PointF position;
+        public double stepX;
+        public double stepY;
 
         public Phisics_Of_Shoot(PointF dir)
         {
             position.X = dir.X;
             position.Y = dir.Y;
+
+            var cursorX = Cursor.Position.X;
+            var cursorY = Cursor.Position.Y;
+
+            var gipotenusa = Math.Sqrt(Math.Abs(cursorX - position.X) * Math.Abs(cursorX - position.X) + Math.Abs(cursorY - position.Y) * Math.Abs(cursorY - position.Y));
+            stepX = Math.Abs(cursorX - position.X) / gipotenusa;
+            stepY = Math.Abs(cursorY - position.Y) / gipotenusa;
         }
 
         public void PlayShoot (Graphics g)
@@ -24,8 +34,10 @@ namespace Shooter.Controllers
 
         public void MakeShoot()
         {
-            position.X += 1;
-            position.Y += 11;
+            
+
+            position.X += (float)stepX;
+            position.Y += (float) stepY;
         }
     }
 }
