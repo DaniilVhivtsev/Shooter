10f69fe37ab540f31cdf32d9656a3979f5ea993a
Даниил Павлович <danya.vshivtsev@mil.ru>
2021-05-17 22:56:13 +0500

    Enemies пули по ним попадают правильно

--- Model/Enemy.cs
+++ Model/Enemy.cs
@@ -47,7 +47,7 @@ namespace Shooter.Model
             else 
                 g.DrawImage(spriteSheetForEnemy, new Rectangle(new Point(Position.X, Position.Y), new Size(31, 31)), 32 * 6, 32 * 4, 31, 31, GraphicsUnit.Pixel);
 
-            g.DrawEllipse(new Pen(Color.Black, 5), Position.X, Position.Y , 5, 5);
+            g.DrawRectangle(new Pen(Color.Black, 2), Position.X, Position.Y, 17, 21);
         }
     }
 }
